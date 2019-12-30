404c6085f5c087d7141553ae9a9c1df7a3ce9cfc
Maadoh Team <hello.maandoh@gmail.com>
2019-12-30 14:35:57 +0800

    Add first script.

---
+++ first_script.py
@@ -0,0 +1,10 @@
+print('This is my first script...')
+
+
+def my_function():
+	print('Hello from my function!')
+
+
+if __name__ == '__main__':
+	print('This program is started from the command line.')
+	my_function()
