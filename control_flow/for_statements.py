f75620a3fce38d6af90fdfe5e2490639bca14cae
Maandoh Team <hello.maandoh@gmail.com>
2020-01-01 14:08:04 +0800

    Ass pass staatements.

--- control_flow/for_statements.py
+++ control_flow/for_statements.py
@@ -1,11 +1,27 @@
+"""
+Python’s for statement iterates over the items of any sequence (a list or a string),
+in the order that they appear in the sequence.
+
+"""
+
+
 def simple_iteration(list_of_items):
 	for word in list_of_items:
 		print(word)
 
+"""
+When using 'enumerate' on a list, you also get the index of the iteration.
+"""
 
 def iterate_with_index(list_of_items):
 	for (i, word) in enumerate(list_of_items):
 		print('Word', i, 'is:', word)
+
+
+"""
+If you do need to iterate over a sequence of numbers, 
+the built-in function range() comes in handy. 
+"""
 
 
 def print_even_numbers():
@@ -15,6 +31,7 @@ def print_even_numbers():
 			continue
 
 		print("Found a number", num)
+
 
 if __name__ == '__main__':
 	words = ['the', 'big', 'brown', 'fox']
---
+++ control_flow/pass_statements.py
@@ -0,0 +1,16 @@
+"""
+The pass statement does nothing.
+It can be used when a statement is required syntactically but the program requires no action.
+"""
+
+
+class MyEmptyClass:
+	pass
+
+
+def initlog(*args):
+	pass  # Remember to implement this!
+
+
+while True:
+	pass  # Busy-wait for keyboard interrupt (Ctrl+C)
