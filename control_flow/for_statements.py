4e645973bbd4f1a866ad43081cb63e7e12703b41
Maandoh Team <hello.maandoh@gmail.com>
2020-01-01 22:52:08 +0800

    Update for statement and first script.

--- control_flow/for_statements.py
+++ control_flow/for_statements.py
@@ -23,6 +23,9 @@ If you do need to iterate over a sequence of numbers,
 the built-in function range() comes in handy. 
 """
 
+"""
+The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+"""
 
 def print_even_numbers():
 	for num in range(2, 10):
@@ -31,10 +34,14 @@ def print_even_numbers():
 			continue
 
 		print("Found a number", num)
+	else:
+		print('Finished printing event numbers.')
 
 
 if __name__ == '__main__':
 	words = ['the', 'big', 'brown', 'fox']
 	simple_iteration(words)
+	simple_iteration('banana')
+
 	iterate_with_index(words)
 	print_even_numbers()
--- first_script.py
+++ first_script.py
@@ -1,9 +1,21 @@
+"""
+Any code that is not contained within this statement will be executed upon running.
+If you’re using your program file as a module, the code that is not in this statement
+will also execute upon its import while running the secondary file.
+"""
+
 print('This is my first script...')
 
 
 def my_function():
 	print('Hello from my function!')
 
+
+"""
+In Python, '__main__' is the name of the scope where top-level code will execute.
+When a program is run from standard input, a script, or from an interactive prompt,
+its __name__ is set equal to '__main__'.
+"""
 
 if __name__ == '__main__':
 	print('This program is started from the command line.')
