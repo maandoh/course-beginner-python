114fc05e30ee7fabcf03ff2c2892d6dfb2ba55ee
Maadoh Team <hello.maandoh@gmail.com>
2020-01-02 20:04:12 +0800

    Updates for list slice notation

---
+++ data_structures/list_slice_notation.py
@@ -0,0 +1,31 @@
+"""
+Python list slice notation in the format:
+
+	list_of_items[start:stop:step]
+"""
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+# Print the last item in the list.
+print(fruits[-1])   # banana
+
+# Print the last 2 items in the list.
+print(fruits[-2:])  # ['apple', 'banana']
+
+# Print everything except the last 2 items.
+print(fruits[:-2])  # ['orange', 'apple', 'pear', 'banana', 'kiwi']
+
+# Print all items in the list, reversed.
+print(fruits[::-1])  # ['banana', 'apple', 'kiwi', 'banana', 'pear', 'apple', 'orange']
+
+# Print the first two items, reversed.
+print(fruits[1::-1])  # ['apple', 'orange']
+
+# Print the last 2 items, reversed.
+print(fruits[:-3:-1])  # ['banana', 'apple']
+
+# Print the last 3 items, reversed.
+print(fruits[:3:-1])  # ['banana', 'apple', 'kiwi']
+
+# Print everything except the last two items, reversed.
+print(fruits[-3::-1])  # ['kiwi', 'banana', 'pear', 'apple', 'orange']
