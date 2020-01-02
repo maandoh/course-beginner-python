7d40b4eb4902f1df42752d1d49dea9429ddd504f
Maandoh Team <hello.maandoh@gmail.com>
2020-01-02 15:08:41 +0800

    Add examples for raising exceptions.

---
+++ raising_exceptions.py
@@ -0,0 +1,11 @@
+"""
+The raise statement forces a specified exception to occur.
+
+Reference:
+https://docs.python.org/3/tutorial/errors.html#handling-exceptions
+"""
+
+
+raise NameError('HiThere')
+
+raise ValueError  # shorthand for 'raise ValueError()'
